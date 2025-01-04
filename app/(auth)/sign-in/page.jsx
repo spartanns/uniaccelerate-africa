b52e5adc930b062d5b69ea41858f2784481a1191
Milos Spartan <milos.djurdjevic.brains24@gmail.com>
2025-01-04 23:51:40 +0100

    Add sign in form

--- app/(auth)/sign-in/page.jsx
+++ app/(auth)/sign-in/page.jsx
@@ -1,5 +1,19 @@
 import AuthForm from "@/components/AuthForm";
+import Image from "next/image";
 
-const SignIn = () => <AuthForm type="sign-in" />;
+const SignIn = () => {
+  return (
+    <section className="flex w-full min-h-screen overflow-hidden justify-between">
+      <AuthForm />
+      <Image
+        src="/bg.png"
+        alt="buildings"
+        width={512}
+        height={512}
+        className="sm:hidden md:block object-cover"
+      />
+    </section>
+  );
+};
 
 export default SignIn;
--- components/AuthForm.jsx
+++ components/AuthForm.jsx
@@ -1,8 +1,11 @@
 "use client";
 
-import { z } from "zod";
+import Image from "next/image";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
+import { z } from "zod";
 
 import { Button } from "@/components/ui/button";
 import {
@@ -17,38 +20,76 @@ import {
 import { Input } from "@/components/ui/input";
 
 const formSchema = z.object({
-  username: z.string().min(4).max(50),
+  email: z.string().min(10).max(50),
+  password: z.string().min(10).max(50),
 });
 
-const AuthForm = ({ type }) => {
+const AuthForm = () => {
   const form = useForm({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      username: "",
+      email: "",
+      password: "",
     },
   });
 
-  const handleSubmit = async (values) => {
+  const onSubmit = (values) => {
     console.log(values);
   };
 
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(handleSubmit)} className="space-y-8">
-        <FormField
-          control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="username" {...field} />
-              </FormControl>
-            </FormItem>
-          )}
-        />
-      </form>
-    </Form>
+    <section className="flex flex-col">
+      <div className="flex justify-start items-center mt-10 ml-5">
+        <Image src="/logo.png" alt="logo" width={32} height={32} />
+        <p>uniaccelerate</p>
+      </div>
+      <Tabs defaultValue="sign-in" className="w-full mt-10 ml-5">
+        <TabsList>
+          <TabsTrigger value="sign-in" className="text-3xl">
+            Sign In
+          </TabsTrigger>
+          <TabsTrigger value="sign-up" className="text-3xl">
+            Sign Up
+          </TabsTrigger>
+        </TabsList>
+        <TabsContent value="sign-in">
+          <p className="text-slate-500">
+            Let's get started by filling out the form below.
+          </p>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input placeholder="email" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input placeholder="password" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </form>
+          </Form>
+        </TabsContent>
+        <TabsContent value="sign-up">Sign up here</TabsContent>
+      </Tabs>
+    </section>
   );
 };
 
